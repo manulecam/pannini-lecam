f13e49640121898ee11a232e0ed850461e167f99
manulecam <lecam.manuel@gmail.com>
2022-10-25 16:23:07 -0300

    Cambio de ubicaciones y detalles visuales en voucher sect

--- js/voucher.js
+++ js/voucher.js
@@ -2,18 +2,19 @@
 
 
 let numeroAleatorio= Math.floor(Math.random()*18)+1;
+
 let fuente1 = `../images/figu${numeroAleatorio}.jpg`;
 document.querySelector('.img1').setAttribute('src', fuente1);
 
 
 
 
-let codigoDescuento= Math.floor(Math.random()*999999999);
+let codigoDescuento= Math.floor(Math.random()*99999999);
 if (fuente1 == "../images/figu1.jpg"){
     document.querySelector('h1').textContent="¡FELICIDADES, GANASTE!";
     document.querySelector('h2').textContent=`Tu código de descuento es: ${codigoDescuento} `;
 }
 else{
-    document.querySelector('h1').textContent="¡PERDISTE!";
+    document.querySelector('h1').textContent="¡MALA SUERTE, PERDISTE!";
     document.querySelector('h2').textContent="Esta vez no salió Messi, intentalo nuevamente...";
 }
