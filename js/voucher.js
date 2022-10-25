7fd5786bddc30766ce83a422bbd219c564c106f8
manulecam <lecam.manuel@gmail.com>
2022-10-25 17:03:01 -0300

    Agregado de transicion a img figurita

--- js/voucher.js
+++ js/voucher.js
@@ -1,8 +1,8 @@
-// ------------------------------------------------------------------------------------------------------------
-// ** Proposito de la funcion: cambiar (la ruta de) las imagenes de las figuritas
-// así tambien como imprimir en pantalla el mensaje correspondiente, según sea el caso
-// ** Parametros: - figuritaAleatoria: numero aleatorio que nos permitirá cambiar la ruta de las imagenes
-//                - codigoDescuento: numero aleatorio del 0 al 9999999 (codigo que por ahora no tiene funcionalidad ni utilidad)
+// ***********************************************************************************************************
+// ***********************************   Función cambiarFigurita();   ****************************************
+// * Proposito de la funcion: cambiar (la ruta de) las imagenes de las figuritas y el mensaje en pantalla
+// * Parametros: - figuritaAleatoria: numero aleatorio que nos permitirá cambiar la ruta de las imagenes
+//               - codigoDescuento: numero aleatorio del 0 al 99999999 (por el momento no tiene utilidad)
 function cambiarFigurita(figuritaAleatoria, codigoDescuento){
     let fuente1 = `../images/figu${figuritaAleatoria}.jpg`;
     document.querySelector('.img1').setAttribute('src', fuente1);
@@ -18,16 +18,16 @@ function cambiarFigurita(figuritaAleatoria, codigoDescuento){
     }
     return fuente1;
 }
-// ------------------------------------------------------------------------------------------------------------
+// ***********************************************************************************************************
 
-// ------------------------------------------------------------------------------------------------------------
+// ***********************************************************************************************************
 // Variable con número aleatorio (1-18) para pasarla como parámetro para el llamado de la funcion (figuritaAleatoria)
 let numeroAleatorio= Math.floor(Math.random()*18)+1;
 // Variable con código de descuento aleatorio para pasarla como parámetro para el llamado de la funcion (codigoDescuento)
 let codigoAleatorio= Math.floor(Math.random()*99999999);
 // Llamado a la función
 cambiarFigurita(numeroAleatorio, codigoAleatorio);
-// ------------------------------------------------------------------------------------------------------------
+// ***********************************************************************************************************
 
 
 
